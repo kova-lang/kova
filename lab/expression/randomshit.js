3e6d92d33a24857f51f8cad6dfa57ebf248bd95e
cybugg <timehinsamuel11@gmail.com>
2026-04-13 15:42:59 +0100

    feat(lab-expression): random  <- experiment with left-nested recursion

--- lab/expression/randomshit.js
+++ lab/expression/randomshit.js
@@ -9,3 +9,20 @@ function dontAsk(layer = 3) {
 
 
 console.log(JSON.stringify(dontAsk(), null, 3));
+
+
+let count = 4;
+
+function oneSideNest(count = 4) {
+    let side1 = { fruit: "Grape", dairy: "cheese", pastry: "bread" };
+    let side2 = { fruit: "Banana", dairy: "milk", pastry: "cake" };
+    while (count > 0) {
+    
+        console.log(count)
+        side1 = { side1, side2 }
+        count--;
+    }
+    return side1;
+}
+
+console.log(JSON.stringify(oneSideNest(), null, 2));
